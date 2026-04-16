95efef0918bcb29eb96850d0bf06ff4cad975dff
Trevy Burgess <TrevyBurgess@live.com>
2026-04-16 10:57:47 -0400

    Change package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.cyberfeedforward.loyaltycardmanager"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.cyberfeedforward.loyaltycardmanager"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
