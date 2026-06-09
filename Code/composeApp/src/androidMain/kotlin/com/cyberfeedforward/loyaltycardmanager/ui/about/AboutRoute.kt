f269f862da8090d4b966781933b48d5f21d00517
Trevy Burgess <IAmThatAm@gmail.com>
2026-06-09 11:16:36 -0400

    Update project structure

--- Code/composeApp/src/androidMain/kotlin/com/cyberfeedforward/loyaltycardmanager/ui/about/AboutRoute.kt
+++ Code/composeApp/src/androidMain/kotlin/com/cyberfeedforward/loyaltycardmanager/ui/about/AboutRoute.kt
@@ -1,11 +1,9 @@
 package com.cyberfeedforward.loyaltycardmanager.ui.about
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -14,14 +12,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.cyberfeedforward.loyaltycardmanager.R
+// import com.loyaltycard.shared.R
 
 @Preview(showBackground = true)
 @Composable
@@ -40,7 +35,7 @@ fun AboutRoute(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start,
         ) {
-            Image(
+            /*Image(
                 painter = painterResource(id = R.drawable.feature_graphic),
                 contentDescription = null,
                 modifier = Modifier
@@ -48,11 +43,11 @@ fun AboutRoute(
                     .align(Alignment.CenterHorizontally)
                     .padding(bottom = 16.dp),
                 contentScale = ContentScale.FillWidth,
-            )
+            )*/
 
             Text(
                 fontSize = 36.sp,
-                text = stringResource(R.string.about),
+                text = "About", //stringResource(R.string.about),
                 color = MaterialTheme.colorScheme.primary,
             )
 
@@ -72,7 +67,7 @@ fun AboutRoute(
 
             Text(
                 fontSize = 36.sp,
-                text = stringResource(R.string.security),
+                text = "Security", //stringResource(R.string.security),
                 color = MaterialTheme.colorScheme.primary,
             )
 
